8794d830aef46cbee6511c019ef0b9337407e64b
Oz Ramos <checkboxoz@gmail.com>
2020-03-11 13:13:21 -0700

    Scroll!

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 let model
 let stream
 let isLooping = false
+let canScroll = false
+let hasSetOrigin = false
+let scrollOrigin = [0, 0]
+let scrollSpeed = 2.5
 const $video = document.querySelector('video')
 const $canvas = document.querySelector('canvas')
 const ctx = $canvas.getContext('2d')
+const $iframe = document.querySelector('iframe')
+const $button = document.querySelector('button')
 
 /**
  * Starts tracking with handpose
  */
 async function startTracking() {
+  $button.innerHTML = 'loading...'
+  $button.setAttribute('disabled', true)
+
   model = await handpose.load()
   getMediaStream()
   isLooping = true
@@ -24,6 +33,7 @@ async function getMediaStream() {
     $video.play()
     $canvas.width = $video.width
     $canvas.height = $video.height
+    $button.remove()
     loop()
   }
 }
@@ -35,13 +45,48 @@ async function loop() {
   const hands = await model.estimateHands($video)
 
   ctx.clearRect(0, 0, $canvas.width, $canvas.height)
-  console.log(hands)
 
   hands.forEach((hand) => {
     drawHand(hand)
+    checkForScrollGesture(hand)
+    scrollPage(hand)
   })
 
   isLooping && requestAnimationFrame(() => isLooping && loop())
+}
+
+/**
+ * Checks for scrolling gesture
+ */
+function checkForScrollGesture(hand) {
+  let thumb = hand.landmarks[4]
+  let pointer = hand.landmarks[8]
+  let a = thumb[0] - pointer[0]
+  let b = thumb[1] - pointer[1]
+  let dist = Math.sqrt(a * a + b * b)
+
+  if (dist < 40) {
+    canScroll = true
+  } else {
+    canScroll = false
+    hasSetOrigin = false
+  }
+
+  if (canScroll && !hasSetOrigin) {
+    hasSetOrigin = true
+    scrollOrigin = [window.scrollY, thumb[1]]
+  }
+}
+
+/**
+ * Scrolls the page
+ */
+function scrollPage(hand) {
+  if (canScroll) {
+    let scroll = (scrollOrigin[1] - hand.landmarks[4][1]) * scrollSpeed
+
+    window.scrollTo(0, scrollOrigin[0] + scroll)
+  }
 }
 
 /**
